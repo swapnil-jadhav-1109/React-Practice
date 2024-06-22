b2e1c53bff1dcfd4d12dd230b532f443e1218de7
swapnil-jadhav-1109 <swapniljadhav4912@gmail.com>
2024-06-22 21:06:42 +0530

    React Practice

--- Demo/src/components/Body.jsx
+++ Demo/src/components/Body.jsx
@@ -3,22 +3,27 @@ import resobj from "../utilities/api";
 import { useState } from "react";
 const Body = () => {
   const [listofReastaurant, setlistofRestaurant] = useState(resobj);
+//  useEffect(()=>{
+//   console.log("upload successfully")
+//  },[]);
   return (
     <div className="body">
       <div className="restaurant-heading">
         <h1>Restaurants with online food delivery in Mumbai</h1>
       </div>
-      {/* <div className="filteredList">
+      <div className="filteredList">
         <button onClick={() => {
           const filtered = listofReastaurant.filter((res) => res.resobj.Food = "North Indian" );
           setlistofRestaurant(filtered);
         }}>
           Top Rated Restaurants
         </button>
-      </div> */}
+      </div>
+
+
       <div className="cards">
         {
-          listofReastaurant.map((restaurant,index) => <Card key={index} data={restaurant} />)
+          listofReastaurant.map((restaurant, index) => <Card key={index} data={restaurant} />)
         }
 
       </div>
