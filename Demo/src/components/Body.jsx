527a221d206e755f8d8a7f56643ac7d9e7e582a5
swapnil-jadhav-1109 <swapniljadhav4912@gmail.com>
2024-06-24 13:31:16 +0530

    React Practice

--- Demo/src/components/Body.jsx
+++ Demo/src/components/Body.jsx
@@ -1,11 +1,17 @@
 import Card from "./Card";
 import resobj from "../utilities/api";
-import { useState } from "react";
+import { useState , useEffect } from "react";
 const Body = () => {
   const [listofReastaurant, setlistofRestaurant] = useState(resobj);
 //  useEffect(()=>{
-//   console.log("upload successfully")
+//   fetchData();
 //  },[]);
+//  const fetchData = async ()=>{
+//   const data = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=19.0759837&lng=72.8776559");
+
+//   const json = await data.json();
+//   console.log(json);
+//  }
   return (
     <div className="body">
       <div className="restaurant-heading">
@@ -13,7 +19,7 @@ const Body = () => {
       </div>
       <div className="filteredList">
         <button onClick={() => {
-          const filtered = listofReastaurant.filter((res) => res.resobj.Food = "North Indian" );
+          const filtered = listofReastaurant.filter((res) => res.resobj.Ratings === "4.1");
           setlistofRestaurant(filtered);
         }}>
           Top Rated Restaurants
