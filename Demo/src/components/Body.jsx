33e5b0daabd0350fdbe44d29f1a25c634b1b1e55
swapnil-jadhav-1109 <swapniljadhav4912@gmail.com>
2024-06-25 14:10:02 +0530

    React Practice

--- Demo/src/components/Body.jsx
+++ Demo/src/components/Body.jsx
@@ -1,32 +1,15 @@
 import Card from "./Card";
 import resobj from "../utilities/api";
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
+import Header from "./Header";
 const Body = () => {
   const [listofReastaurant, setlistofRestaurant] = useState(resobj);
-//  useEffect(()=>{
-//   fetchData();
-//  },[]);
-//  const fetchData = async ()=>{
-//   const data = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=19.0759837&lng=72.8776559");
-
-//   const json = await data.json();
-//   console.log(json);
-//  }
+  
   return (
     <div className="body">
       <div className="restaurant-heading">
         <h1>Restaurants with online food delivery in Mumbai</h1>
       </div>
-      <div className="filteredList">
-        <button onClick={() => {
-          const filtered = listofReastaurant.filter((res) => res.resobj.Ratings === "4.1");
-          setlistofRestaurant(filtered);
-        }}>
-          Top Rated Restaurants
-        </button>
-      </div>
-
-
       <div className="cards">
         {
           listofReastaurant.map((restaurant, index) => <Card key={index} data={restaurant} />)
--- Demo/src/components/Header.jsx
+++ Demo/src/components/Header.jsx
@@ -1,25 +1,36 @@
-  import {LOGO_URL} from '../utilities/constants'
+import { LOGO_URL } from '../utilities/constants'
+import { useState } from 'react';
+import resobj from "../utilities/api";
 const Header = () => {
-    return (
-      <div className="header">
-        <div className="logo-container">
-          <img  className="logo" src={LOGO_URL} />
+  const [Search, setSearch] = useState(resobj);
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
 
-        </div>
-    
-        <div className="nav-items">
-          <ul>
-           <a href="" className="nav-links"> <li>Home</li></a>
-           <a href="" className="nav-links"> <li>About Us</li></a>
-            <a href="" className="nav-links"><li>Contact Us</li></a>
-           <a href="" className="nav-links"> <li>Cart</li></a>
-          </ul>
-        </div>
-        <div className="search-container">
-          <input type="search" placeholder='search Here' className='searchInput' />
-          <button className='searchButton'>Search</button>
-        </div>
       </div>
-    )
-  }
-  export default Header;
+
+      <div className="nav-items">
+        <ul>
+          <a href="" className="nav-links"> <li>Home</li></a>
+          <a href="" className="nav-links"> <li>About Us</li></a>
+          <a href="" className="nav-links"><li>Contact Us</li></a>
+          <a href="" className="nav-links"> <li>Cart</li></a>
+        </ul>
+      </div>
+      <div className="search-container">
+        <input type="search" placeholder='search Here' className='searchInput' />
+        <button className='searchButton' onClick={() => {
+          const Searched = Search.filter((res) => {
+            if (resobj.foodName === "VIG") {
+              console.log("this food is present to the cafe");
+            }
+          }
+          );
+          setSearch(Searched);
+        }}>Search</button>
+      </div>
+    </div>
+  )
+}
+export default Header;
