19db37dd95bd068510a15d6cb4f710082ac19a22
swapnil-jadhav-1109 <swapniljadhav4912@gmail.com>
2024-06-27 15:37:34 +0530

    Generate OTP

---
+++ OTP Generate/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState ,useRef } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+ const x = 0;
+
+  return (
+    <>
+    <input
+    autoFocus
+    />
+    <input/>
+    <input/>
+    <input/>
+
+     </>
+  )
+}
+
+export default App
