08ebcf0cfed24c238adb0f3e34d2cbd1c71b37dc
swapnil-jadhav-1109 <swapniljadhav4912@gmail.com>
2024-07-12 21:31:27 +0530

    React Practice

--- Demo/src/components/Toprestaurant.jsx
+++ Demo/src/components/Toprestaurant.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { useState,useEffect } from 'react';
 import ShimerUi from './ShimerUi';
-import Card from './Card'
+import Card from './Card';
 import { FaArrowLeft } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa6";
 
 
 const Toprestaurant = () => {
     const [listofTopRestaurant, setlistofTopRestaurant] = useState([]);
+    const [slide,setslide] = useState(0);
     const [loading, setLoading] = useState(true);
+
   
     useEffect(() => {
       fetchData();
@@ -24,6 +26,18 @@ const Toprestaurant = () => {
   
     if (loading) {
       return <ShimerUi />;
+    }
+    const nextslide = ()=>{
+      console.log(listofTopRestaurant.length);
+      console.log(slide - 2);
+      if(slide == 0) return false;
+      setslide(slide + 1)
+    }
+    const prevslide = ()=>{
+      console.log(listofTopRestaurant.length);
+      console.log(slide + 2);
+      if(slide == -10) return false;
+      setslide(slide - 1)
     }
   
     return (
@@ -41,12 +55,12 @@ const Toprestaurant = () => {
         </div> */}
         <div className="Top-Restaurants">
         <div className=" slider-arrow">
-        <p className='arrow'>< FaArrowLeft/></p>
-        <p className='arrow'><FaArrowRight /></p>
+        <p className='arrow' onClick={nextslide}>< FaArrowLeft/></p>
+        <p className='arrow'  onClick={prevslide}><FaArrowRight /></p>
         </div>
          <div  className="Top-Restaurants-cards">
           {listofTopRestaurant.map((restaurant, index) => (
-            <Card key={index} data={restaurant} />
+           <div key={index} className='top-cards shrink-0 duration-500' style ={{transform: `translateX(${slide}00%)` }}> <Card  data={restaurant} /></div>
           ))}
           </div>
         </div>
@@ -54,3 +68,62 @@ const Toprestaurant = () => {
     );
   };
 export default Toprestaurant;
+
+// import React, { useState, useEffect } from 'react';
+// import ShimerUi from './ShimerUi';
+// import Card from './Card';
+// import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
+// const Toprestaurant = () => {
+//   const [listofTopRestaurant, setlistofTopRestaurant] = useState([]);
+//   const [slide, setslide] = useState(0);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   const fetchData = async () => {
+//     const response = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=17.6804639&lng=74.018261");
+//     const json = await response.json();
+//     console.log(json?.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+//     setlistofTopRestaurant(json?.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants || []);
+//     setLoading(false);
+//   };
+
+//   if (loading) {
+//     return <ShimerUi />;
+//   }
+
+//   const prevslide = () => {
+//     setslide(slide - 800);
+//   };
+
+//   const nextslide = () => {
+//     setslide(slide + 800);
+//   };
+
+//   return (
+//     <div className="body">
+//       <div className="restaurant-heading">
+//         <h1>Top Restaurant Chains in Satara</h1>
+//       </div>
+//       <div className="Top-Restaurants">
+//         <div className="slider-arrow">
+//           <p className='arrow' onClick={prevslide}><FaArrowLeft /></p>
+//           <p className='arrow' onClick={nextslide}><FaArrowRight /></p>
+//         </div>
+//         <div className="Top-Restaurants-cards">
+//           {listofTopRestaurant.map((restaurant, index) => (
+//             <div key={index} className='top-cards shrink-0 duration-500' style={{ transform: `translateX(${slide}px)` }}>
+//               <Card data={restaurant} />
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Toprestaurant;
+
