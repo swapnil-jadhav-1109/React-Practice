40d809bfbfc2347699999618197b1aa60360c6d3
swapnil-jadhav-1109 <swapniljadhav4912@gmail.com>
2024-07-20 14:48:24 +0530

    Swiggy Website clone

--- Demo/src/components/Menu.jsx
+++ Demo/src/components/Menu.jsx
@@ -1,10 +1,84 @@
-const Menu = ()=>{
+// import { useEffect, useState } from "react";
+// import ShimerUi from "./ShimerUi";
+
+// const Menu = () => {
+
+//     const [resInfo, setResInfo] = useState(null);
+//     useEffect(() => {
+//         resMenu();
+//     }, []);
+
+//     const resMenu = async () => {
+//         const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.6804639&lng=74.018261&restaurantId=155959&catalog_qa=undefined&submitAction=ENTER");
+//         const json = await data.json();
+//         console.log(json);
+//         setResInfo(json.data);
+
+//         if (resInfo === null) return <ShimerUi />
+
+//         const { name, cuisines, costForTwoMessage } = resInfo.cards[2]?.card.card.info;
+//         console.log(resInfo.cards[2]?.card.card.info);
+//         const itemCards = resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+
+//         console.log(itemCards);
+//     }
+//     return (
+//         <>
+//           <h1> {name} </h1>
+//           <p> {cuisines.join(" , ")} - {costForTwoMessage} </p>
+//            {
+//             itemCards.map((item)=>{
+//                 return  <li key={item.card.info.id}>
+//                 {item.card.info.name} - {"Rs."}
+//                 {item.card.info.price/100}
+//             </li>
+                  
+//             })
+//            }
+//         </>
+//     )
+// }
+// export default Menu;
+
+import { useEffect, useState } from "react";
+import ShimerUi from "./ShimerUi";
+
+const Menu = () => {
+    const [resInfo, setResInfo] = useState(null);
+
+    useEffect(() => {
+        const resMenu = async () => {
+            try {
+                const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.6804639&lng=74.018261&restaurantId=155959&catalog_qa=undefined&submitAction=ENTER");
+                const json = await data.json();
+                console.log(json);
+                setResInfo(json.data);
+            } catch (error) {
+                console.error("Error fetching menu data:", error);
+            }
+        };
+        resMenu();
+    }, []);
+
+    if (resInfo === null) return <ShimerUi />;
+
+    const { name, cuisines, costForTwoMessage } = resInfo.cards[2]?.card.card.info || {};
+    const itemCards = resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+    console.log(itemCards);
     return (
         <>
-        <div className="Menu">
-        <p className="menu-para">Home/Satara/</p>
-        </div>
+            <h1>{name}</h1>
+            <p>{cuisines?.join(" , ")} - {costForTwoMessage}</p>
+            <ul>
+                {
+            itemCards.map((item)=>{
+            
+            })
+           
+                }
+                 </ul>
         </>
-    )
-}
+    );
+};
+
 export default Menu;
