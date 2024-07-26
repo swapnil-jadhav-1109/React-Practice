3da77af39d380eab8833cee35ff7c6d471a2f050
swapnil-jadhav-1109 <swapniljadhav4912@gmail.com>
2024-07-26 15:17:49 +0530

    Add to the code in the website

--- Demo/src/components/ItemList.jsx
+++ Demo/src/components/ItemList.jsx
@@ -27,13 +27,14 @@ const ItemList = ({ items }) => {
         <div className='itemMenu'>
             {items.map((item) => (
                 <div key={item.card.info.id} className='items'>
-                    <div>
+                    <div className='iteminfo'>
                         <p className='Itemlist'>{item.card.info.name}</p>
                         <p className='ItemlistPrice'>₹ {item.card.info.price / 100}</p>
                         <p className='ItemlistDescription'>{item.card.info.description}</p>
                     </div>
                     <div >
                        <img className='img-itemlist' src={IMG_PATH + item.card.info.imageId}  alt="" />
+                       <button className='img-btn'><h2>ADD</h2></button>
                     </div>
                 </div>
 
--- Demo/src/components/ResCategory.jsx
+++ Demo/src/components/ResCategory.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import { IoIosArrowDown } from "react-icons/io";
+import React, { useState } from 'react'
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import ItemList from './ItemList';
 
 const ResCategory = ({ data }) => {
+  const [show , setShow] = useState(false);
+
+  const handleClick = ()=>{
+    setShow(!show);
+
+  }
+
   return (
     <div>
       <div className='Categories' >
-        <div className='category'>
+        <div className='category' onClick={handleClick}>
           <span className='title'>{data.title} ({data.itemCards.length})</span>
-          <span className='title title-arrow'>< IoIosArrowDown /></span>
+          <span className='title title-arrow'>
+         {show ?< IoIosArrowUp />:< IoIosArrowDown />} 
+        </span>
         </div>
-       <ItemList items={data.itemCards}/>
+      { show && <ItemList items={data.itemCards}/>}
       </div>
     </div>
   )
