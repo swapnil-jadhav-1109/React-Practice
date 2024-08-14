131ef05361a0a4f9fbce464dccaab112950f9acd
swapnil-jadhav-1109 <swapniljadhav4912@gmail.com>
2024-08-14 20:39:29 +0530

    Some changes

--- Demo/src/components/CartInfo.jsx
+++ Demo/src/components/CartInfo.jsx
@@ -1,39 +1,9 @@
-// import React from 'react'
-// import ItemList from './ItemList'
-// const CartInfo = () => {
-//     // const {name,price,description} = props.item.card.info;
-//   return (
-//     <div className='itemMenu'>
-//             {items.map((item) => (
-//                 <div key={item.card.info.id} className='items'>
-//                     <div className='iteminfo'>
-//                         <p className='Itemlist'>{item.card.info.name}</p>
-//                         <p className='ItemlistPrice'>₹ {item.card.info.price / 100 || item.card.info.defaultPrice/100}</p>
-//                         <p className='ItemlistDescription'>{item.card.info.description}</p>
-//                     </div>
-//                     <div >
-//                        <img className='img-itemlist' src={IMG_PATH + item.card.info.imageId}  alt="" />
-                       
-//                     </div>
-//                 </div>
 
-//             ))}
-//         </div>
-//   )
-// }
-
-// export default CartInfo
 
 import React from 'react';
 import { IMG_PATH } from '../utilities/constants';
-import { useDispatch } from 'react-redux';
-import { addItems } from '../utilities/cartSlice';
 const CartInfo = ({ items }) => {
-   
-     const dispatch = useDispatch();
-    // const HandleClickAdd = (item)=>{
-    //     dispatch(addItems(item));
-    // }
+
 
     console.log(items);
     return (
@@ -47,8 +17,7 @@ const CartInfo = ({ items }) => {
                     </div>
                     <div >
                        <img className='img-itemlist' src={IMG_PATH + item.card.info.imageId}  alt="" />
-                       {/* <button className='img-btn' onClick={()=>HandleClickAdd(item)} ><h2>ADD</h2></button> */}
-                    </div>
+                           </div>
                 </div>
 
             ))}
